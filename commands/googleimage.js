159304e98a9f4b6de2219b7e4c605702d7027671
itsreimau <private.iblmln@gmail.com>
2024-02-12 18:19:57 +0700

    add new commands

---
+++ commands/googleimage.js
@@ -0,0 +1,39 @@
+const {
+    bold
+} = require('@mengkodingan/ckptw');
+const fg = require('api-dylux');
+
+module.exports = {
+    name: 'googleimage',
+    aliases: ['gimage'],
+    category: 'internet',
+    code: async (ctx) => {
+        const input = ctx._args.join(' ');
+
+        if (!input) return ctx.reply(`${bold('[ ! ]')} Masukkan kueri!`);
+
+        try {
+            const gimage = await fg.googleImage(input);
+
+            await ctx.reply({
+                image: {
+                    url: getRandomElement(gimage)
+                },
+                caption: `• Kueri: ${input}`
+            });
+        } catch (error) {
+            console.error("Error:", error);
+            return ctx.reply(`${bold('[ ! ]')} Terjadi kesalahan: ${error.message}`);
+        }
+    }
+};
+
+function getRandomElement(arr) {
+    if (arr.length === 0) {
+        return undefined;
+    }
+
+    const randomIndex = Math.floor(Math.random() * arr.length);
+
+    return arr[randomIndex];
+}
