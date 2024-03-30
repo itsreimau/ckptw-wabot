b122591c97c67b526caab17f2ad282593d9d0dd9
itsreimau <private.iblmln@gmail.com>
2024-03-30 15:03:47 +0700

    anu

--- commands/owner-bcgc.js
+++ commands/owner-bcgc.js
@@ -24,14 +24,15 @@ module.exports = {
         if (!isOwner(ctx)) return ctx.reply(global.msg.owner);
 
         try {
-            const getGroups = await ctx._client.groupFetchAllParticipating()
-            const groups = Object.entries(getGroups).slice(0).map(entry => entry[1])
-            const anu = groups.map(v => v.id)
+            const delay = time => new Promise(res => setTimeout(res, time));
+            const getGroups = await ctx._client.groupFetchAllParticipating();
+            const groups = Object.entries(getGroups).slice(0).map(entry => entry[1]);
+            const anu = groups.map(v => v.id);
 
-            ctx.reply(`Mengirim siaran ke ${anu.length} obrolan grup, perkiraan waktu penyelesaian adalah ${anu.length * 0,5} detik.`)
+            ctx.reply(`Mengirim siaran ke ${anu.length} obrolan grup, perkiraan waktu penyelesaian adalah ${anu.length * 0,5} detik.`);
 
             for (let i of anu) {
-                await delay(500)
+                await delay(500);
                 ctx.sendMessage(i, {
                     text: input,
                     contextInfo: {
@@ -49,7 +50,7 @@ module.exports = {
                 });
             }
 
-            ctx.reply(`Berhasil mengirimkan siaran ke ${anu.length} obrolan grup.`)
+            ctx.reply(`Berhasil mengirimkan siaran ke ${anu.length} obrolan grup.`);
         } catch (error) {
             console.error('Error:', error);
             return ctx.reply(`${bold('[ ! ]')} Terjadi kesalahan: ${error.message}`);
