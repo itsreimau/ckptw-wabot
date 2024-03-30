047a2959d4820ecff3194e7f918d75b6d1b7802a
itsreimau <private.iblmln@gmail.com>
2024-03-30 11:48:16 +0700

    small changes

--- commands/info-donate.js
+++ commands/info-donate.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'donate',
-    aliases: ['donasi']
+    aliases: ['donasi'],
     category: 'info',
     code: async (ctx) => {
         return ctx.sendMessage(ctx.id, {
