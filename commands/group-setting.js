45f72749b2a4d538d415868e893de14fcced1379
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-04-09 00:35:57 +0700

    update commands/group-setting.js

--- commands/group-setting.js
+++ commands/group-setting.js
@@ -1,6 +1,7 @@
 const {
     isNotAdmin,
-    isNotAdminOf
+    isNotAdminOf,
+    isPrivate
 } = require('../handler.js');
 const {
     bold,
@@ -22,7 +23,7 @@ module.exports = {
 
         if (isNotAdminOf(ctx)) return ctx.reply(global.msg.botAdmin);
 
-        if (!ctx.isGroup()) return ctx.reply(global.msg.group);
+        if (isPrivate(ctx)) return ctx.reply(global.msg.group);
 
         try {
             const isClose = {
