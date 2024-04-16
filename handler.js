4c4736a55f663e045d45148437338a411c7cd499
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-04-16 23:52:09 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -5,36 +5,20 @@ const smpl = require('./lib/simple.js');
  * @param {Object} ctx The context of the request.
  * @param {Object} options The given options, including 'opt'.
  */
-exports.handler = (ctx, {
-    opt
-}) => {
-    let isAdmin = smpl.isAdmin(ctx);
-    let isOwner = smpl.isOwner(ctx);
+exports.handler = (ctx, options) => {
+    const isAdmin = smpl.isAdmin(ctx);
+    const isOwner = smpl.isOwner(ctx);
 
-    if (isAdmin === 0 && isOwner === 0) {
-        ctx.reply(global.msg[opt]);
-    } else {
-        let result;
-        switch (opt) {
-            case 'admin':
-                result = isAdmin;
-                break;
-            case 'botAdmin':
-                result = smpl.isAdminOf(ctx);
-                break;
-            case 'group':
-                result = smpl.isGroup(ctx);
-                break;
-            case 'owner':
-                result = isOwner;
-                break;
-            case 'private':
-                result = smpl.isPrivate(ctx);
-                break;
-            default:
-                throw new Error('Invalid option provided');
-        }
+    if (options.admin && isAdmin === 0) ctx.reply(global.msg['admin']);
 
-        if (result === 0) ctx.reply(global.msg[opt]);
-    }
+    if (options.botAdmin && smpl.isAdminOf(ctx) === 0) ctx.reply(global.msg['botAdmin']);
+
+    if (options.group && smpl.isGroup(ctx) === 0) ctx.reply(global.msg['group']);
+
+    if (options.owner && isOwner === 0) ctx.reply(global.msg['owner']);
+
+    if (options.private && smpl.isPrivate(ctx) === 0) ctx.reply(global.msg['private']);
+
+    if (Object.values(options).every(value => !value)) throw new Error('Invalid option provided');
+
 }
