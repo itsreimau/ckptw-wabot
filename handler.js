ca5d34cdaef49d77359fb307b4068c5aae51b5f7
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-04-16 22:42:23 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -1,49 +1,30 @@
-exports.isOwner = (ctx) => {
-    const isOwner = ctx._sender.jid.includes(global.owner.number);
-    return isOwner;
-}
+const smpl = require('./lib/simple.js');
 
-exports.isNotOwner = (ctx) => {
-    const isOwner = ctx._sender.jid.includes(global.owner.number);
-    return !isOwner;
-}
-
-exports.isAdmin = async (ctx) => {
-    const isAdmin = await checkAdmin(ctx, ctx._sender.jid.split('@')[0]);
-    return isAdmin;
-}
-
-exports.isNotAdmin = async (ctx) => {
-    const isAdmin = await checkAdmin(ctx, ctx._sender.jid.split('@')[0]);
-    return !isAdmin;
-}
-
-exports.isAdminOf = async (ctx) => {
-    const isAdminOfGroup = await checkAdmin(ctx, ctx._client.user.id.split(':')[0]);
-    return isAdminOfGroup;
-}
-
-exports.isNotAdminOf = async (ctx) => {
-    const isAdminOfGroup = await checkAdmin(ctx, ctx._client.user.id.split(':')[0]);
-    return !isAdminOfGroup;
-}
-
-exports.isGroup = (ctx) => {
-    return ctx.isGroup();
-}
-
-exports.isPrivate = (ctx) => {
-    return !ctx.isGroup();
-}
-
-async function checkAdmin(ctx, id) {
-    const groupMetadata = await ctx._client.groupMetadata(ctx.id);
-
-    if (groupMetadata) {
-        const participants = groupMetadata.participants;
-        const participant = participants.find(participant => participant.id.split('@')[0] === id);
-        return participant && participant.admin === 'admin';
-    } else {
-        return false;
+/**
+ * Handles requests based on the given option.
+ * @param {Object} ctx The context of the request.
+ * @param {Object} options The given options, including 'opt'.
+ */
+exports.handler = (ctx, {
+    opt
+}) => {
+    let result;
+    switch (opt) {
+        case 'admin':
+            result = smpl.isAdmin(ctx);
+            break;
+        case 'botAdmin':
+            result = smpl.isAdminOf(ctx);
+            break;
+        case 'group':
+            result = smpl.isGroup(ctx);
+            break;
+        case 'owner':
+            result = smpl.isOwner(ctx);
+            break;
+        default:
+            throw new Error('Invalid option provided');
     }
+
+    if (result === 0) ctx.reply(global.msg[opt]);
 }
