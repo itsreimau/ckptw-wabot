20696dc8416703367a4a6607bc00d01df6f0a02e
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-04-17 13:49:28 +0700

    update handler.js

--- handler.js
+++ handler.js
@@ -9,15 +9,15 @@ exports.handler = (ctx, options) => {
     const isAdmin = smpl.isAdmin(ctx);
     const isOwner = smpl.isOwner(ctx);
 
-    if (options.admin && isAdmin === 0) ctx.reply(global.msg.admin);
+    if (options.admin && isAdmin === 0) return ctx.reply(global.msg.admin);
 
-    if (options.botAdmin && smpl.isAdminOf(ctx) === 0) ctx.reply(global.msg.botAdmin);
+    if (options.botAdmin && smpl.isAdminOf(ctx) === 0) return ctx.reply(global.msg.botAdmin);
 
-    if (options.group && smpl.isGroup(ctx) === 0) ctx.reply(global.msg.group);
+    if (options.group && smpl.isGroup(ctx) === 0) return ctx.reply(global.msg.group);
 
-    if (options.owner && isOwner === 0) ctx.reply(global.msg.owner);
+    if (options.owner && isOwner === 0) return ctx.reply(global.msg.owner);
 
-    if (options.private && smpl.isPrivate(ctx) === 0) ctx.reply(global.msg.private);
+    if (options.private && smpl.isPrivate(ctx) === 0) return ctx.reply(global.msg.private);
 
     if (Object.values(options).every(value => !value)) throw new Error('Invalid option provided');
 }
