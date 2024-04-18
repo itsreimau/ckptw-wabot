eb32dfe0a3f597926e7ca5163d1b54f2ef882fd5
Muhamad Ikbal Maulana | ItsReimau <private.iblmln@gmail.com>
2024-04-18 14:20:11 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -8,17 +8,22 @@ const smpl = require('./lib/simple.js');
  */
 exports.handler = (ctx, options) => {
     const isAdmin = smpl.isAdmin(ctx);
+    const isAdminOf = smpl.isAdminOf(ctx);
+    const isGroup = smpl.isGroup(ctx);
     const isOwner = smpl.isOwner(ctx);
+    const isPrivate = smpl.isPrivate(ctx);
 
-    if (options.private && smpl.isPrivate(ctx) === 0) return global.msg.private;
+    if (options.private && isPrivate === 0) return global.msg.private;
 
-    if (options.group && smpl.isGroup(ctx) === 0) return global.msg.group;
+    if (options.group && isGroup === 0) return global.msg.group;
+
+    if (options.admin && options.owner && isAdmin === 0) return global.msg.admin;
 
     if (options.owner && isOwner === 0 && !options.botAdmin) return global.msg.owner;
 
     if (options.admin && isAdmin === 0) return global.msg.admin;
 
-    if (options.botAdmin && smpl.isAdminOf(ctx) === 0 && !options.owner) return global.msg.botAdmin;
+    if (options.botAdmin && isAdminOf === 0 && !options.owner) return global.msg.botAdmin;
 
     return null;
 }
