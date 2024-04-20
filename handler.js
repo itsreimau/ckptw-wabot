e752f102a3bce74959eb15f1848ba5bb5f12eb79
itsreimau <private.iblmln@gmail.com>
2024-04-20 16:05:57 +0700

    small changes

--- handler.js
+++ handler.js
@@ -7,23 +7,20 @@ const smpl = require('./lib/simple.js');
  * @returns {string|null} Message if applicable, otherwise null.
  */
 exports.handler = (ctx, options) => {
-    const isAdmin = smpl.isAdmin(ctx);
-    const isAdminOf = smpl.isAdminOf(ctx);
-    const isGroup = smpl.isGroup(ctx);
-    const isOwner = smpl.isOwner(ctx);
     const isPrivate = smpl.isPrivate(ctx);
-
     if (options.private && isPrivate === 0) return global.msg.private;
 
-    if (options.group && isGroup === 0) return global.msg.group;
+    const isGroup = smpl.isGroup(ctx);
+    else if (options.group && isGroup === 0) return global.msg.group;
 
-    if (options.admin && options.owner && isAdmin === 0) return global.msg.admin;
+    const isOwner = smpl.isOwner(ctx);
+    else if (options.owner && isOwner === 0) return global.msg.owner;
 
-    if (options.owner && isOwner === 0) return global.msg.owner;
+    const isAdmin = smpl.isAdmin(ctx);
+    else if (options.admin && isAdmin === 0) return global.msg.admin;
 
-    if (options.admin && isAdmin === 0) return global.msg.admin;
-
-    if (options.botAdmin && isAdminOf === 0) return global.msg.botAdmin;
+    const isAdminOf = smpl.isAdminOf(ctx);
+    else if (options.botAdmin && isAdminOf === 0) return global.msg.botAdmin;
 
     return null;
 }
