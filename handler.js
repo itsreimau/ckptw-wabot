f1d82352d5764d7283ded1324fc2c0f667f6e726
itsreimau <private.iblmln@gmail.com>
2024-04-20 16:59:16 +0700

    small changes

--- handler.js
+++ handler.js
@@ -4,32 +4,46 @@ const smpl = require('./lib/simple.js');
  * Handles requests based on the given options.
  * @param {Object} ctx The context of the request.
  * @param {Object} options The given options.
- * @returns {Object} Object containing status, option, and message if applicable, otherwise null.
+ * @returns {Object} Object containing status and message if applicable, otherwise null.
  */
 exports.handler = (ctx, options) => {
-    const checkFunctions = {
-        admin: () => smpl.isAdmin(ctx) === 0,
-        botAdmin: () => smpl.isAdminOf(ctx) === 0,
-        group: () => smpl.isGroup(ctx) === 0,
-        owner: () => smpl.isOwner(ctx) === 0,
-        private: () => smpl.isPrivate(ctx) === 0
+    const checkOptions = {
+        admin: {
+            function: () => smpl.isAdmin(ctx) === 0,
+            msg: global.msg.admin
+        },
+        botAdmin: {
+            function: () => smpl.isAdminOf(ctx) === 0,
+            msg: global.msg.botAdmin
+        },
+        group: {
+            function: () => smpl.isGroup(ctx) === 0,
+            msg: global.msg.group
+        },
+        owner: {
+            function: () => smpl.isOwner(ctx) === 0,
+            msg: global.msg.owner
+        },
+        private: {
+            function: () => smpl.isPrivate(ctx) === 0,
+            msg: global.msg.private
+        }
     };
 
-    const validOptions = [];
+    let status = false;
+    let message = null;
 
     for (const option of Object.keys(options)) {
-        if (checkFunctions[option] && checkFunctions[option]() && global.msg[option]) validOptions.push(option);
+        const checkOption = checkOptions[option];
+        if (checkOption.function()) {
+            status = true;
+            message = checkOption.msg;
+            break;
+        }
     }
 
-    if (validOptions.length > 0) {
-        return {
-            status: true,
-            option: validOptions,
-            msg: global.msg[validOptions[0]]
-        };
-    } else {
-        return {
-            status: false
-        };
-    }
+    return {
+        status,
+        message
+    };
 }
