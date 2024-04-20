69ac835e200f53c6745e5f9e07036111f581d75f
itsreimau <private.iblmln@gmail.com>
2024-04-20 16:07:59 +0700

    small changes

--- handler.js
+++ handler.js
@@ -11,16 +11,16 @@ exports.handler = (ctx, options) => {
     if (options.private && isPrivate === 0) return global.msg.private;
 
     const isGroup = smpl.isGroup(ctx);
-    else if (options.group && isGroup === 0) return global.msg.group;
+    if (options.group && isGroup === 0) return global.msg.group;
 
     const isOwner = smpl.isOwner(ctx);
-    else if (options.owner && isOwner === 0) return global.msg.owner;
+    if (options.owner && isOwner === 0) return global.msg.owner;
 
     const isAdmin = smpl.isAdmin(ctx);
-    else if (options.admin && isAdmin === 0) return global.msg.admin;
+    if (options.admin && isAdmin === 0) return global.msg.admin;
 
     const isAdminOf = smpl.isAdminOf(ctx);
-    else if (options.botAdmin && isAdminOf === 0) return global.msg.botAdmin;
+    if (options.botAdmin && isAdminOf === 0) return global.msg.botAdmin;
 
     return null;
 }
