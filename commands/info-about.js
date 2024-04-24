80e6b08d8724fc22180ebdd283cd4491c814eff6
itsreimau <private.iblmln@gmail.com>
2024-04-24 17:33:34 +0700

    q

--- commands/info-about.js
+++ commands/info-about.js
@@ -12,7 +12,7 @@ module.exports = {
             '\n' +
             `Halo! Saya Rei Ayanami, bot WhatsApp yang terinspirasi oleh karakter anime 'Neon Genesis Evangelion'. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!\n` +
             `${global.msg.readmore}\n` +
-            quote('Penafian: Rei Ayanami (bot WhatsApp) adalah proyek kreatif oleh Muhamad Ikbal Maulana dan bukan merupakan bagian resmi dari seri 'Neon Genesis Evangelion' atau perusahaan terkait lainnya.')
+            quote('Penafian: Rei Ayanami (bot WhatsApp) adalah proyek kreatif oleh Muhamad Ikbal Maulana dan bukan merupakan bagian resmi dari seri "Neon Genesis Evangelion" atau perusahaan terkait lainnya.')
         ); // Dapat diubah sesuai keinginan Anda
     }
 };
