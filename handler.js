fbfe0bef77127912fe8f63737089371d3f1b1bde
itsreimau <private.iblmln@gmail.com>
2024-04-25 16:17:20 +0700

    s

--- handler.js
+++ handler.js
@@ -13,7 +13,7 @@ exports.handler = async (ctx, options) => {
             msg: global.msg.admin
         },
         banned: {
-            function: async () => await global.db.get(`user.${ctx._sender.jid.split('@')[0])}.isBanned`),
+            function: async () => await global.db.get(`user.${ctx._sender.jid.split('@')[0]}.isBanned`),
             msg: global.msg.banned
         },
         botAdmin: {
