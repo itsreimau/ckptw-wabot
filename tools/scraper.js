3f68529051583cd3c4d977f5c0bf683606a7d009
itsreimau <private.iblmln@gmail.com>
2024-05-11 11:39:23 +0700

    fix: scraper

--- tools/scraper.js
+++ tools/scraper.js
@@ -294,75 +294,94 @@ exports.pinterest = async (query) => {
 
 /**
  * Convert WebP image to MP4 video using ezgif.com.
- * @param {buffer|string} source The source image as a buffer or a URL string.
+ * @param {Buffer|string} source The source image as a buffer or a URL string.
  * @returns {string} The URL of the converted MP4 video.
  */
 exports.webp2mp4 = async (source) => {
-    let form = new FormData()
-    let isUrl = typeof source === 'string' && /https?:\/\//.test(source)
-    const blob = !isUrl && new Blob([source.toArrayBuffer()])
-    form.append('new-image-url', isUrl ? blob : '')
-    form.append('new-image', isUrl ? '' : blob, 'image.webp')
+    let form = new FormData();
+    let isUrl = typeof source === 'string' && /^https?:\/\//.test(source);
+
+    if (!isUrl && typeof source !== 'string') source = Buffer.from(source);
+
+    form.append('new-image-url', isUrl ? source : '');
+    form.append('new-image', isUrl ? '' : source, 'image.webp');
+
     let res = await fetch('https://ezgif.com/webp-to-mp4', {
         method: 'POST',
         body: form,
-    })
-    let html = await res.text()
+    });
+
+    let html = await res.text();
     let {
         document
-    } = new JSDOM(html).window
-    let form2 = new FormData()
-    let obj = {}
+    } = new JSDOM(html).window;
+    let form2 = new FormData();
+    let obj = {};
+
     for (let input of document.querySelectorAll('form input[name]')) {
-        obj[input.name] = input.value
-        form2.append(input.name, input.value)
+        obj[input.name] = input.value;
+        form2.append(input.name, input.value);
     }
+
     let res2 = await fetch('https://ezgif.com/webp-to-mp4/' + obj.file, {
         method: 'POST',
         body: form2,
-    })
-    let html2 = await res2.text()
+    });
+
+    let html2 = await res2.text();
     let {
         document: document2
-    } = new JSDOM(html2).window
+    } = new JSDOM(html2).window;
+
     return new URL(
         document2.querySelector('div#output > p.outfile > video > source').src,
         res2.url
-    ).toString()
+    ).toString();
 }
 
 /**
  * Convert WebP image to PNG image using ezgif.com.
- * @param {buffer|string} source The source image as a buffer or a URL string.
+ * @param {Buffer|string} source The source image as a buffer or a URL string.
  * @returns {string} The URL of the converted PNG image.
  */
 exports.webp2png = async (source) => {
-    let form = new FormData()
-    let isUrl = typeof source === 'string' && /https?:\/\//.test(source)
-    const blob = !isUrl && new Blob([source.toArrayBuffer()])
-    form.append('new-image-url', isUrl ? blob : '')
-    form.append('new-image', isUrl ? '' : blob, 'image.webp')
+    let form = new FormData();
+    let isUrl = typeof source === 'string' && /^https?:\/\//.test(source);
+
+    if (!isUrl && typeof source !== 'string') source = Buffer.from(source);
+
+    form.append('new-image-url', isUrl ? source : '');
+    form.append('new-image', isUrl ? '' : source, 'image.webp');
+
     let res = await fetch('https://ezgif.com/webp-to-png', {
         method: 'POST',
         body: form,
-    })
-    let html = await res.text()
+    });
+
+    let html = await res.text();
     let {
         document
-    } = new JSDOM(html).window
-    let form2 = new FormData()
-    let obj = {}
+    } = new JSDOM(html).window;
+    let form2 = new FormData();
+    let obj = {};
+
     for (let input of document.querySelectorAll('form input[name]')) {
-        obj[input.name] = input.value
-        form2.append(input.name, input.value)
+        obj[input.name] = input.value;
+        form2.append(input.name, input.value);
     }
+
     let res2 = await fetch('https://ezgif.com/webp-to-png/' + obj.file, {
         method: 'POST',
         body: form2,
-    })
-    let html2 = await res2.text()
+    });
+
+    let html2 = await res2.text();
     let {
         document: document2
-    } = new JSDOM(html2).window
-    return new URL(document2.querySelector('div#output > p.outfile > img').src, res2.url).toString()
+    } = new JSDOM(html2).window;
+
+    return new URL(
+        document2.querySelector('div#output > p.outfile > img').src,
+        res2.url
+    ).toString();
 }
