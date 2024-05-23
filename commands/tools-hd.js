6943ff2f669dfbafbfffd44070143f6481aaee92
itsreimau <private.iblmln@gmail.com>
2024-05-23 14:47:27 +0700

    fix: 30sec

--- commands/tools-hd.js
+++ commands/tools-hd.js
@@ -44,7 +44,7 @@ module.exports = {
             });
 
             const response = await axios.get(apiUrl, {
-                timeout: 60000 // 60 seconds timeout
+                timeout: 30000 // 30 seconds timeout
             });
 
             if (response.status !== 200) throw new Error(global.msg.notFound);
