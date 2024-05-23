b86bc334710d1fe3e92c86151b5d7b4a6b158f78
itsreimau <private.iblmln@gmail.com>
2024-05-23 13:49:19 +0700

    fix: apiUrl

--- commands/tools-hd.js
+++ commands/tools-hd.js
@@ -33,12 +33,13 @@ module.exports = {
         const quotedMessage = ctx._msg.message?.extendedTextMessage?.contextInfo?.quotedMessage;
 
         if (msgType !== MessageType.imageMessage && !quotedMessage) return ctx.reply(`${bold("[ ! ]")} Berikan atau balas media berupa gambar!`);
+
         try {
             const type = quotedMessage ? ctx._self.getContentType(quotedMessage) : null;
             const object = type ? quotedMessage[type] : null;
             const buffer = type === "imageMessage" ? await download(object, type.slice(0, -7)) : await ctx.getMediaMessage(ctx._msg, "buffer");
             const uplRes = await uploadByBuffer(buffer, mime.contentType("png"));
-            const apiUrl = await createAPIUrl("ngodingaja", "/api/hd", {
+            const apiUrl = createAPIUrl("ngodingaja", "/api/hd", {
                 url: uplRes.link
             });
             const response = await axios.get(apiUrl);
@@ -55,7 +56,7 @@ module.exports = {
                 caption: null
             });
         } catch (error) {
-            console.error("Error:", error);
+            console.error("Error", error);
             return ctx.reply(`${bold("[ ! ]")} Terjadi kesalahan: ${error.message}`);
         }
     }
