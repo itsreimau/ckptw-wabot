0b57b0ddc2ca3249fb01e945a772432286c83076
itsreimau <private.iblmln@gmail.com>
2024-06-12 17:16:22 +0700

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     apps: [{
-        name: "rei-ayanami",
+        name: "ckptw-wabot",
         script: "./index.js",
         watch: true,
         ignore_watch: ["database.json", "node_modules", "state"]
