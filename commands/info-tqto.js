1a0282c1affa39e9e33672fcf4603bca25de70a2
itsreimau <private.iblmln@gmail.com>
2024-08-14 17:35:16 +0700

    fix

--- commands/info-tqto.js
+++ commands/info-tqto.js
@@ -3,15 +3,15 @@ const {
 } = require("@mengkodingan/ckptw");
 
 module.exports = {
-        name: "tqto",
-        aliases: ["thanksto"],
-        category: "info",
-        code: async (ctx) => {
-                return ctx.reply(
-                        `${quote("Allah SWT"`)}\n` +
+    name: "tqto",
+    aliases: ["thanksto"],
+    category: "info",
+    code: async (ctx) => {
+        return ctx.reply(
+            `${quote("Allah SWT")}\n` +
             `${quote("Serv00 (https://serv00.com/)")}\n` +
-           `${quote( "Idul (https://github.com/aidulcandra)"`)}\n` +
-           `${quote( "JastinXyz (https://github.com/JastinXyz)"`)}\n` +
+            `${quote("Idul (https://github.com/aidulcandra)")}\n` +
+            `${quote("JastinXyz (https://github.com/JastinXyz)")}\n` +
             `${quote("Dan kepada semua pihak yang telah membantu dalam pengembangan bot ini.")}\n` +
             "\n" +
             global.msg.footer
