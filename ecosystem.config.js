b69555465367334865244d705320429807adc097
itsreimau <private.iblmln@gmail.com>
2024-08-29 17:11:25 +0700

    fuck

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,10 +7,6 @@ module.exports = {
         // Advanced features.
         watch: true,
         ignore_watch: ["database.json", "node_modules", "state"],
-        max_memory_restart: "300M",
-
-        // Log files.
-        log_date_format: "YYYY-MM-DD HH:mm Z",
 
         // Control flow.
         restart_delay: 5000,
