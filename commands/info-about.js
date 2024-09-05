e5c1e526bd4dd2ed5f49800555e00feafe988014
itsreimau <private.iblmln@gmail.com>
2024-09-05 23:15:49 +0700

    fix about

--- commands/info-about.js
+++ commands/info-about.js
@@ -10,6 +10,6 @@ module.exports = {
             global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
         ]);
 
-        return ctx.reply(await global.tools.msg.translate(`Halo! Saya adalah Bot WhatsApp bernama ${global.bot.name}, dimiliki oleh ${global.owner.name}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`, userLanguage) {}); // Can be changed according to your wishes.
+        return ctx.reply(await global.tools.msg.translate(`Halo! Saya adalah Bot WhatsApp bernama ${global.bot.name}, dimiliki oleh ${global.owner.name}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`, userLanguage)); // Can be changed according to your wishes.
     }
 };
