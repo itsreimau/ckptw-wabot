345959265836f586d67c4b09f039202c06e1e9c9
itsreimau <private.iblmln@gmail.com>
2024-09-05 23:17:15 +0700

    fix speed cmds

--- commands/info-speed.js
+++ commands/info-speed.js
@@ -6,22 +6,21 @@ const {
 } = require("perf_hooks");
 
 module.exports = {
-        name: "speed",
-        category: "info",
-        code: async (ctx) => {
-            const [userLanguage] = await Promise.all([
-                global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
-            ]);
+    name: "speed",
+    category: "info",
+    code: async (ctx) => {
+        const [userLanguage] = await Promise.all([
+            global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
+        ]);
 
-            try {
-                const startTime = performance.now();
-                const testSpeed = await ctx.reply(quote(`🔄 ${await global.tools.msg.translate("Menguji kecepatan...", userLanguage)}`));
-                const responseTime = (performance.now() - startTime).toFixed(2);
-                await ctx.editMessage(testSpeed.key, quote(`🚀 ${await global.tools.msg.translate(`Merespon dalam ${responseTime} ms.`, userLanguage)})`);
-                }
-                catch (error) {
-                    console.error("Error:", error);
-                    return ctx.reply(quote(`⚠ ${await global.tools.msg.translate("Terjadi kesalahan", userLanguage)}: ${error.message}`));
-                }
-            }
-        };
+        try {
+            const startTime = performance.now();
+            const testSpeed = await ctx.reply(quote(`🔄 ${await global.tools.msg.translate("Menguji kecepatan...", userLanguage)}`));
+            const responseTime = (performance.now() - startTime).toFixed(2);
+            await ctx.editMessage(testSpeed.key, quote(`🚀 ${await global.tools.msg.translate(`Merespon dalam ${responseTime} ms.`, userLanguage)})`));
+        } catch (error) {
+            console.error("Error:", error);
+            return ctx.reply(quote(`⚠ ${await global.tools.msg.translate("Terjadi kesalahan", userLanguage)}: ${error.message}`));
+        }
+    }
+};
