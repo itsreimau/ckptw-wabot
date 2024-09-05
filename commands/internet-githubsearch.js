11d94de87aa9052980d6ad33f8c4b315f00b2b68
itsreimau <private.iblmln@gmail.com>
2024-09-05 23:22:05 +0700

    fix githubsearch cmds

--- commands/internet-githubsearch.js
+++ commands/internet-githubsearch.js
@@ -30,7 +30,7 @@ module.exports = {
         );
 
         try {
-            const apiUrl = await await global.tools.api.createUrl("agatz", "/api/github", {
+            const apiUrl = await global.tools.api.createUrl("agatz", "/api/github", {
                 message: input
             });
             const response = await axios.get(apiUrl);
