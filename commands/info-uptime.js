964ff5cc7c18299bace7b5de5dbe8657d51af0f3
itsreimau <private.iblmln@gmail.com>
2024-09-05 23:19:15 +0700

    fix uptime cmds

--- commands/info-uptime.js
+++ commands/info-uptime.js
@@ -3,24 +3,22 @@ const {
 } = require("@mengkodingan/ckptw");
 
 module.exports = {
-        name: "uptime",
-        category: "info",
-        code: async (ctx) => {
-                const [userLanguage] = await Promise.all([
-                    global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
-                ]);
+    name: "uptime",
+    category: "info",
+    code: async (ctx) => {
+        const [userLanguage] = await Promise.all([
+            global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
+        ]);
+        const {
+            status,
+            message
+        } = await global.handler(ctx, {
+            banned: true
+        });
+        if (status) return ctx.reply(message);
 
-                const {
-                    status,
-                    message
-                } = await global.handler(ctx, {
-                    banned: true
-                });
-                if (status) return ctx.reply(message);
+        const uptime = global.tools.general.convertMsToDuration(Date.now() - global.system.startTime) || "kurang dari satu detik.";
 
-                const uptime = global.tools.general.convertMsToDuration(Date.now() - global.system.startTime) || "kurang dari satu detik.";
-                return ctx.reply(quote(`🚀 ${await global.tools.msg.translate(Bot telah aktif selama ${uptime}.`, userLanguage)
-                }
-                `));
+        return ctx.reply(quote(`🚀 ${await global.tools.msg.translate(`Bot telah aktif selama ${uptime}.`, userLanguage)}`));
     }
 };
