1e14f7ff670b98f343152dce9f68f693b55913f7
itsreimau <private.iblmln@gmail.com>
2024-09-06 00:19:57 +0700

    fix ping

--- commands/info-ping.js
+++ commands/info-ping.js
@@ -2,18 +2,6 @@ module.exports = {
     name: "ping",
     category: "info",
     code: async (ctx) => {
-        const [userLanguage] = await Promise.all([
-            global.db.get(`user.${ctx.sender.jid.replace(/@.*|:.*/g, "")}.language`)
-        ]);
-
-        const {
-            status,
-            message
-        } = await global.handler(ctx, {
-            banned: true
-        });
-        if (status) return ctx.reply(message);
-
         return ctx.reply("Pong!");
     }
 };
