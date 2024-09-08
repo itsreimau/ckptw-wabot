acec3759d1cfa9cdb0349d88d88e7deb53502133
itsreimau <private.iblmln@gmail.com>
2024-09-08 12:33:14 +0700

    fix gemini

--- commands/ai-gemini.js
+++ commands/ai-gemini.js
@@ -33,39 +33,38 @@ module.exports = {
         if (!input) return ctx.reply(
             `${quote(`📌 ${await global.tools.msg.translate(global.msg.argument, userLanguage)}`)}\n` +
             `${quote(`${await global.tools.msg.translate("Contoh", userLanguage)}: ${monospace(`${ctx._used.prefix + ctx._used.command} apa itu whatsapp?`)}`)}\n` +
-            quote(await global.tools.msg.translate("Catatan: AI ini dapat melihat gambar dan menjawab pertanyaan tentangnya. Kirim gambar dan tanyakan apa saja!", userLanguage)
-            })
-    );
+            quote(await global.tools.msg.translate("Catatan: AI ini dapat melihat gambar dan menjawab pertanyaan tentangnya. Kirim gambar dan tanyakan apa saja!", userLanguage))
+        );
 
-    const msgType = ctx.getMessageType();
+        const msgType = ctx.getMessageType();
 
-    try {
-        if (msgType !== MessageType.imageMessage || msgType !== MessageType.videoMessage || !ctx.quoted?.conversation) {
-            const apiUrl = global.tools.api.createUrl("sandipbaruwal", "/gemini", {
-                prompt: input
-            });
-            const {
-                data
-            } = await axios.get(apiUrl);
+        try {
+            if (msgType !== MessageType.imageMessage || msgType !== MessageType.videoMessage || !ctx.quoted?.conversation) {
+                const apiUrl = global.tools.api.createUrl("sandipbaruwal", "/gemini", {
+                    prompt: input
+                });
+                const {
+                    data
+                } = await axios.get(apiUrl);
 
-            return ctx.reply(data.answer);
-        } else {
-            const buffer = await ctx.msg.media.toBuffer() || await ctx.quoted?.media.toBuffer();
-            const uploadResponse = await uploadByBuffer(buffer, mime.lookup("png"));
-            const apiUrl = global.tools.api.createUrl("sandipbaruwal", `/gemini2`, {
-                prompt: input,
-                url: uploadResponse.link
-            });
-            const {
-                data
-            } = await axios.get(apiUrl);
+                return ctx.reply(data.answer);
+            } else {
+                const buffer = await ctx.msg.media.toBuffer() || await ctx.quoted?.media.toBuffer();
+                const uploadResponse = await uploadByBuffer(buffer, mime.lookup("png"));
+                const apiUrl = global.tools.api.createUrl("sandipbaruwal", `/gemini2`, {
+                    prompt: input,
+                    url: uploadResponse.link
+                });
+                const {
+                    data
+                } = await axios.get(apiUrl);
 
-            return ctx.reply(data.answer);
+                return ctx.reply(data.answer);
+            }
+        } catch (error) {
+            console.error("Error:", error);
+            if (error.status !== 200) return ctx.reply(`⛔ ${await global.tools.msg.translate(global.msg.notFound, userLanguage)}`);
+            return ctx.reply(quote(`⚠ ${await global.tools.msg.translate("Terjadi kesalahan", userLanguage)}: ${error.message}`));
         }
-    } catch (error) {
-        console.error("Error:", error);
-        if (error.status !== 200) return ctx.reply(`⛔ ${await global.tools.msg.translate(global.msg.notFound, userLanguage)}`);
-        return ctx.reply(quote(`⚠ ${await global.tools.msg.translate("Terjadi kesalahan", userLanguage)}: ${error.message}`));
     }
-}
 };
