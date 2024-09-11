dfff14bb1392278bee79c052ac0c30c3b358debd
itsreimau <private.iblmln@gmail.com>
2024-09-11 22:50:39 +0700

    fix tools

--- tools/exports.js
+++ tools/exports.js
@@ -1,7 +1,7 @@
-const global.tools = {
+const tools = {
     api: require("./api.js"),
     general: require("./general.js"),
     list: require("./list.js")
 };
 
-module.exports = global.tools;
+module.exports = tools;
