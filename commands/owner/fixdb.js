f9e5dbcbfdd5937dc6a40993c92c2b46aa04db4d
itsreimau <private.iblmln@gmail.com>
2024-11-17 17:04:25 +0700

    [Update] Added destructuring for objects and variables in fixdb.js to improve code readability.

--- commands/owner/fixdb.js
+++ commands/owner/fixdb.js
@@ -17,7 +17,9 @@ module.exports = {
             const waitMsg = await ctx.reply(config.msg.wait);
             const oneMonthAgo = Date.now() - (30 * 24 * 60 * 60 * 1000);
             const dbJSON = await db.toJSON();
-            const { user = {}, group = {}, menfess = {} } = dbJSON;
+            const {
+                user = {}, group = {}, menfess = {}
+            } = dbJSON;
 
             const userImportantKeys = ["afk", "coin", "isBanned", "isPremium", "lastClaim", "lastUse", "level", "winGame", "xp"];
             const afkKeys = ["reason", "timeStamp"];
@@ -26,7 +28,7 @@ module.exports = {
             const optionKeys = ["antilinkgc", "antitoxic", "autokick", "welcome"];
             const textKeys = ["goodbye", "intro", "welcome"];
 
-            const batchSize = 100; 
+            const batchSize = 100;
             const processBatch = async (data, processFunc) => {
                 const keys = Object.keys(data);
                 for (let i = 0; i < keys.length; i += batchSize) {
@@ -37,7 +39,12 @@ module.exports = {
 
             await ctx.editMessage(waitMsg.key, quote(`🔄 Memproses data pengguna...`));
             await processBatch(user, async (userId) => {
-                const { lastUse, afk = {}, lastClaim = {}, ...userData } = user[userId] || {};
+                const {
+                    lastUse,
+                    afk = {},
+                    lastClaim = {},
+                    ...userData
+                } = user[userId] || {};
 
                 if (!/^[0-9]{10,15}$/.test(userId) || (lastUse && new Date(lastUse).getTime() < oneMonthAgo)) {
                     db.delete(`user.${userId}`);
@@ -46,13 +53,20 @@ module.exports = {
                     const filteredLastClaim = Object.fromEntries(Object.entries(lastClaim).filter(([key]) => lastClaimKeys.includes(key)));
                     const filteredData = Object.fromEntries(Object.entries(userData).filter(([key]) => userImportantKeys.includes(key)));
 
-                    db.set(`user.${userId}`, { ...filteredData, afk: filteredAfk, lastClaim: filteredLastClaim, lastUse });
+                    db.set(`user.${userId}`, {
+                        ...filteredData,
+                        afk: filteredAfk,
+                        lastClaim: filteredLastClaim,
+                        lastUse
+                    });
                 }
             });
 
             await ctx.editMessage(waitMsg.key, quote(`🔄 Memproses data grup...`));
             await processBatch(group, async (groupId) => {
-                const { option = {}, text = {}, ...groupData } = group[groupId] || {};
+                const {
+                    option = {}, text = {}, ...groupData
+                } = group[groupId] || {};
 
                 if (!/^[0-9]{10,15}$/.test(groupId)) {
                     db.delete(`group.${groupId}`);
@@ -61,13 +75,20 @@ module.exports = {
                     const filteredText = Object.fromEntries(Object.entries(text).filter(([key]) => textKeys.includes(key)));
                     const filteredGroupData = Object.fromEntries(Object.entries(groupData).filter(([key]) => groupImportantKeys.includes(key)));
 
-                    db.set(`group.${groupId}`, { ...filteredGroupData, option: filteredOption, text: filteredText });
+                    db.set(`group.${groupId}`, {
+                        ...filteredGroupData,
+                        option: filteredOption,
+                        text: filteredText
+                    });
                 }
             });
 
             await ctx.editMessage(waitMsg.key, quote(`🔄 Memproses data menfess...`));
             await processBatch(menfess, async (conversationId) => {
-                const { lastMsg, ...menfessData } = menfess[conversationId] || {};
+                const {
+                    lastMsg,
+                    ...menfessData
+                } = menfess[conversationId] || {};
 
                 if (lastMsg && new Date(lastMsg).getTime() < oneMonthAgo) {
                     db.delete(`menfess.${conversationId}`);
