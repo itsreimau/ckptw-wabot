3cb9fc6e8ef19c894761a06fea35814364d03649
itsreimau <private.iblmln@gmail.com>
2024-11-28 20:57:05 +0700

    [Update] updated comments and message strings to improve code readability and maintainability.

--- commands/downloader/threadsdl.js
+++ commands/downloader/threadsdl.js
@@ -5,59 +5,60 @@ const axios = require("axios");
 const mime = require("mime-types");
 
 module.exports = {
-        name: "threadsdl",
-        aliases: ["threads"],
-        category: "downloader",
-        handler: {
-            coin: [10, "text", 1]
-        },
-        code: async (ctx) => {
-            const status = await handler(ctx, module.exports.handler);
-            if (status) return;
+    name: "threadsdl",
+    aliases: ["threads"],
+    category: "downloader",
+    handler: {
+        coin: [10, "text", 1]
+    },
+    code: async (ctx) => {
+        const status = await handler(ctx, module.exports.handler);
+        if (status) return;
 
-            const url = ctx.args[0] || null;
+        const url = ctx.args[0] || null;
 
-            if (!url) return await ctx.reply(
-                `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
-                quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/"))
-            );
+        if (!url) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/"))
+        );
 
-            const isUrl = await tools.general.isUrl(url);
-            if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
+        const isUrl = await tools.general.isUrl(url);
+        if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
 
-            try {
-                const apiUrl = tools.api.createUrl("agatz", "/api/threads", {
-                    url
-                });
-                const {
-                    data
-                } = (await axios.get(apiUrl)).data;
+        try {
+            const apiUrl = tools.api.createUrl("agatz", "/api/threads", {
+                url
+            });
+            const {
+                data
+            } = (await axios.get(apiUrl)).data;
 
-                if (data.image_urls && data.image_urls.length > 0) {
-                    for (const imageUrl of data.image_urls) {
-                        await ctx.reply({
-                            image: {
-                                url: imageUrl
-                            },
-                            mimetype: mime.lookup("png")
-                        });
-                    }
-
-                    if (data.video_urls && data.video_urls.length > 0) {
-                        for (const videoUrl of data.video_urls) {
-                            await ctx.reply({
-                                video: {
-                                    url: videoUrl
-                                },
-                                mimetype: mime.lookup("mp4"),
-                                gifPlayback: false
-                            });
-                        }
-                    }
-                } catch (error) {
-                    console.error(`[${config.pkg.name}] Error:`, error);
-                    if (error.status !== 200) return await ctx.reply(config.msg.notFound);
-                    return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+            if (data.image_urls && data.image_urls.length > 0) {
+                for (const imageUrl of data.image_urls) {
+                    await ctx.reply({
+                        image: {
+                            url: imageUrl
+                        },
+                        mimetype: mime.lookup("png"),
+                    });
                 }
             }
-        };
+
+            if (data.video_urls && data.video_urls.length > 0) {
+                for (const videoUrl of data.video_urls) {
+                    await ctx.reply({
+                        video: {
+                            url: videoUrl
+                        },
+                        mimetype: mime.lookup("mp4"),
+                        gifPlayback: false,
+                    });
+                }
+            }
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
+};
--- commands/downloader/tiktokdl.js
+++ commands/downloader/tiktokdl.js
@@ -5,109 +5,110 @@ const axios = require("axios");
 const mime = require("mime-types");
 
 module.exports = {
-        name: "tiktokdl",
-        aliases: ["tiktok", "tiktoknowm", "tt", "ttdl", "vt", "vtdl", "vtdltiktok", "vtnowm"],
-        category: "downloader",
-        handler: {
-            coin: [10, "text", 1]
-        },
-        code: async (ctx) => {
-                const status = await handler(ctx, module.exports.handler);
-                if (status) return;
+    name: "tiktokdl",
+    aliases: ["tiktok", "tiktoknowm", "tt", "ttdl", "vt", "vtdl", "vtdltiktok", "vtnowm"],
+    category: "downloader",
+    handler: {
+        coin: [10, "text", 1]
+    },
+    code: async (ctx) => {
+        const status = await handler(ctx, module.exports.handler);
+        if (status) return;
 
-                const input = ctx.args.join(" ") || null;
+        const input = ctx.args.join(" ") || null;
 
-                if (!input) return await ctx.reply(
-                    `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
-                    `${quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/ -a"))}\n` +
-                    quote(tools.msg.generatesFlagInformation({
-                        "-a": "Otomatis kirim audio.",
-                        "-m": "Unduh musik."
-                    }))
-                );
+        if (!input) return await ctx.reply(
+            `${quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
+            `${quote(tools.msg.generateCommandExample(ctx._used.prefix + ctx._used.command, "https://example.com/ -a"))}\n` +
+            quote(tools.msg.generatesFlagInformation({
+                "-a": "Otomatis kirim audio.",
+                "-m": "Unduh musik."
+            }))
+        );
 
-                const flag = tools.general.parseFlag(input, {
-                    "-a": {
-                        type: "boolean",
-                        key: "audio"
+        const flag = tools.general.parseFlag(input, {
+            "-a": {
+                type: "boolean",
+                key: "audio"
+            },
+            "-m": {
+                type: "boolean",
+                key: "music"
+            }
+        });
+
+        const url = flag.input || null;
+
+        const isUrl = await tools.general.isUrl(url);
+        if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
+
+        try {
+            const mediaType = flag.audio ? "audio" : "video_image";
+
+            const apiUrl = tools.api.createUrl("https://api.tiklydown.eu.org", "/api/download", {
+                url
+            });
+            const {
+                data
+            } = await axios.get(apiUrl);
+
+            if (mediaType === "audio") {
+                return await ctx.reply({
+                    audio: {
+                        url: data.music.play_url
                     },
-                    "-m": {
-                        type: "boolean",
-                        key: "music"
-                    }
+                    mimetype: mime.lookup("mp3")
                 });
+            }
 
-                const url = flag.input || null;
-
-                const isUrl = await tools.general.isUrl(url);
-                if (!isUrl) return await ctx.reply(config.msg.urlInvalid);
-
-                try {
-                    const mediaType = flag.audio ? "audio" : "video_image";
-
-                    const apiUrl = tools.api.createUrl("https://api.tiklydown.eu.org", "/api/download", {
-                        url
+            if (mediaType === "video_image") {
+                if (data.video?.noWatermark) {
+                    return await ctx.reply({
+                        video: {
+                            url: data.video.noWatermark
+                        },
+                        mimetype: mime.lookup("mp4"),
+                        caption: `${quote(`URL: ${url}`)}\n` +
+                            "\n" +
+                            config.msg.footer,
+                        gifPlayback: false
                     });
-                    const {
-                        data
-                    } = await axios.get(apiUrl);
+                }
 
-                    if (mediaType === "audio") {
-                        return await ctx.reply({
-                            audio: {
-                                url: data.music.play_url
+                if (data.images && data.images.length > 0) {
+                    for (const image of data.images) {
+                        await ctx.reply({
+                            image: {
+                                url: image.url
                             },
-                            mimetype: mime.lookup("mp3")
+                            mimetype: mime.lookup("png")
                         });
                     }
+                }
+            }
 
-                    if (mediaType === "video_image") {
-                        if (data.video?.noWatermark) {
-                            return await ctx.reply({
-                                video: {
-                                    url: data.video.noWatermark
-                                },
-                                mimetype: mime.lookup("mp4"),
-                                caption: `${quote(`URL: ${url}`)}\n` +
-                                    "\n" +
-                                    config.msg.footer,
-                                gifPlayback: false
-                            });
-                        }
+            if (flag.music) {
+                const apiUrl = tools.api.createUrl("vreden", "/api/tikmusic", {
+                    url
+                });
+                const {
+                    result
+                } = (await axios.get(apiUrl)).data;
 
-                        if (data.images && data.images.length > 0) {
-                            for (const image of data.images) {
-                                await ctx.reply({
-                                        image: {
-                                            url: image.url
-                                        },
-                                        mimetype: mime.lookup("png")
-                                    }
-                                }
-                            }
-
-                            if (flag.music) {
-                                const apiUrl = tools.api.createUrl("vreden", "/api/tikmusic", {
-                                    url
-                                });
-                                const {
-                                    result
-                                } = (await axios.get(apiUrl)).data;
-
-                                return await ctx.reply({
-                                    audio: {
-                                        url: data.url
-                                    },
-                                    mimetype: mime.lookup("mp3"),
-                                    caption: `${quote(`URL: ${url}`)}\n` +
-                                        "\n" +
-                                        config.msg.footer
-                                });
-                            }
-                        } catch (error) {
-                            console.error(`[${config.pkg.name}] Error:`, error);
-                            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
-                            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
-                        }
-                    }
-                };
+                return await ctx.reply({
+                    audio: {
+                        url: data.url
+                    },
+                    mimetype: mime.lookup("mp3"),
+                    caption: `${quote(`URL: ${url}`)}\n` +
+                        "\n" +
+                        config.msg.footer
+                });
+            }
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            if (error.status !== 200) return await ctx.reply(config.msg.notFound);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
+};
