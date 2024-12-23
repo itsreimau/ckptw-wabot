3e05c3b05eae18d1c9db9ba8bb27b7b8ec0bcf8d
itsreimau <private.iblmln@gmail.com>
2024-12-23 12:19:34 +0700

    [Add] added command to set bot profile picture in group.

---
+++ commands/owner/setbotpp.js
@@ -0,0 +1,36 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+const {
+    MessageType
+} = require("@mengkodingan/ckptw/lib/Constant");
+
+module.exports = {
+    name: "setbotpp",
+    aliases: ["setboticon", "seticonbot", "setppbot"],
+    category: "owner",
+    handler: {
+        owner: true
+    },
+    code: async (ctx) => {
+        if (await handler(ctx, module.exports.handler)) return;
+
+        const msgType = ctx.getMessageType();
+        const [checkMedia, checkQuotedMedia] = await Promise.all([
+            tools.general.checkMedia(msgType, "image", ctx),
+            tools.general.checkQuotedMedia(ctx.quoted, "image")
+        ]);
+
+        if (!checkMedia && !checkQuotedMedia) return await ctx.reply(quote(tools.msg.generateInstruction(["send", "reply"], "image")));
+
+        try {
+            const buffer = await ctx.msg.media.toBuffer() || await ctx.quoted?.media.toBuffer();
+            await ctx._client.updateProfilePicture(ctx._client.user.id, buffer);
+
+            return await ctx.reply(quote(`✅ Berhasil mengubah gambar profil foto grup!`));
+        } catch (error) {
+            console.error(`[${config.pkg.name}] Error:`, error);
+            return await ctx.reply(quote(`⚠️ Terjadi kesalahan: ${error.message}`));
+        }
+    }
+};
