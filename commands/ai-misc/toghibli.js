d999f1cede0b9682c90eb70d6fae11c193903205
itsreimau <private.iblmln@gmail.com>
2025-04-04 14:08:47 +0700

    feat: Add toghibli command and update join command

---
+++ commands/ai-misc/toghibli.js
@@ -0,0 +1,39 @@
+const {
+    quote
+} = require("@mengkodingan/ckptw");
+const mime = require("mime-types");
+
+module.exports = {
+    name: "toghibli",
+    category: "ai-misc",
+    permissions: {
+        coin: 10
+    },
+    code: async (ctx) => {
+        const msgType = ctx.getMessageType();
+        const [checkMedia, checkQuotedMedia] = await Promise.all([
+            tools.cmd.checkMedia(msgType, "image"),
+            tools.cmd.checkQuotedMedia(ctx.quoted, "image")
+        ]);
+
+        if (!checkMedia && !checkQuotedMedia) return await ctx.reply(quote(tools.cmd.generateInstruction(["send", "reply"], "image")));
+
+        try {
+            const buffer = await ctx.msg.media.toBuffer() || await ctx.quoted.media.toBuffer();
+            const uploadUrl = await tools.general.upload(buffer, "image");
+            const result = tools.api.createUrl("fast", "/aiimage/ghibli", {
+                imageUrl: uploadUrl,
+                type: "png"
+            });
+
+            return await ctx.reply({
+                image: {
+                    url: result
+                },
+                mimetype: mime.lookup("png")
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
+};
--- commands/owner/join.js
+++ commands/owner/join.js
@@ -22,14 +22,14 @@ module.exports = {
 
         try {
             const urlCode = new URL(url).pathname.split("/").pop();
-            const [joinResult] = await ctx.groups.acceptInvite(urlCode);
-            if (joinResult.data === 400) return await ctx.reply(quote(`❎ Grup tidak ditemukan.`));
-            if (joinResult.data === 401) return await ctx.reply(quote(`❎ Bot telah dikeluarkan dari grup itu.`));
-            if (joinResult.data === 409) return await ctx.reply(quote(`❎ Bot telah bergabung dengan grup.`));
-            if (joinResult.data === 410) return await ctx.reply(quote(`❎ URL grup telah disetel ulang.`));
-            if (joinResult.data === 500) return await ctx.reply(quote(`❎ Grup penuh!`));
+            const [result] = await ctx.groups.acceptInvite(urlCode);
+            if (result.data === 400) return await ctx.reply(quote(`❎ Grup tidak ditemukan.`));
+            if (result.data === 401) return await ctx.reply(quote(`❎ Bot telah dikeluarkan dari grup itu.`));
+            if (result.data === 409) return await ctx.reply(quote(`❎ Bot telah bergabung dengan grup.`));
+            if (result.data === 410) return await ctx.reply(quote(`❎ URL grup telah disetel ulang.`));
+            if (result.data === 500) return await ctx.reply(quote(`❎ Grup penuh!`));
 
-            await ctx.sendMessage(res, {
+            await ctx.sendMessage(result, {
                 text: quote(`👋 Halo! Saya adalah Bot WhatsApp bernama ${config.bot.name}, dimiliki oleh ${config.owner.name}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`)
             });
 
