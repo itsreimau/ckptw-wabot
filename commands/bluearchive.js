cd6dd0bce8d38ae8c5ebb206e72455c9e9cd4c81
Rizky Pratama | Kyluxx <155633236+Kyluxx@users.noreply.github.com>
2025-04-27 17:52:25 +0700

    Added bluearchive.js with Pre-Test

---
+++ commands/bluearchive.js
@@ -0,0 +1,24 @@
+const mime = require("mime-types");
+
+module.exports = {
+    name: "bluearchive",
+    aliases: ["ba", "bac"],
+    category: "entertainment",
+    permissions: {
+        coin: 10
+    },
+    code: async (ctx) => {
+        try {
+            const result = tools.api.createUrl("archive", "/api/random/bluearchive");
+
+            return await ctx.reply({
+                image: {
+                    url: result
+                },
+                mimetype: mime.lookup("png")
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
+}
