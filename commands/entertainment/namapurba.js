c40a4e2c4093f10ddd9eb277346e522f2931a8a7
itsreimau <private.iblmln@gmail.com>
2025-04-27 10:35:21 +0700

    fix: Improve namapurba command's code formatting and version bump

--- commands/entertainment/namapurba.js
+++ commands/entertainment/namapurba.js
@@ -3,25 +3,26 @@ const {
 } = require("@mengkodingan/ckptw");
 
 module.exports = {
-        name: "namapurba",
-        aliases: ["purba"],
-        category: "entertainment",
-        permissions: {
-            coin: 5
-        },
-        code: async (ctx) => {
-            const input = ctx.args.join(" ") || null;
+    name: "namapurba",
+    aliases: ["purba"],
+    category: "entertainment",
+    permissions: {
+        coin: 5
+    },
+    code: async (ctx) => {
+        const input = ctx.args.join(" ") || null;
 
-            if (!input) return await ctx.reply(
-                `${quote(tools.cmd.generateInstruction(["send"], ["text"]))}\n` +
-                quote(tools.cmd.generateCommandExample(ctx.used, "itsreimau"))
-            );
+        if (!input) return await ctx.reply(
+            `${quote(tools.cmd.generateInstruction(["send"], ["text"]))}\n` +
+            quote(tools.cmd.generateCommandExample(ctx.used, "itsreimau"))
+        );
 
-            try {
-                const result = input.replace(/[aiueo]/gi, "$&ve");
+        try {
+            const result = input.replace(/[aiueo]/gi, "$&ve");
 
-                return await ctx.reply(quote(result));
-            } catch (error) {
-                return await tools.cmd.handleError(ctx, error, false);
-            }
-        };
+            return await ctx.reply(quote(result));
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, false);
+        }
+    }
+};
