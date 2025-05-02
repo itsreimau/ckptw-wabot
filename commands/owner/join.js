ca460178d954ef4b08800cbab27d3d450ef2c861
itsreimau <private.iblmln@gmail.com>
2025-05-02 15:05:17 +0700

    fix: Correctly await group invite acceptance

--- commands/owner/join.js
+++ commands/owner/join.js
@@ -22,7 +22,7 @@ module.exports = {
 
         try {
             const urlCode = new URL(url).pathname.split("/").pop();
-            await ctx.groups.acceptInvite(urlCode).then(result => {
+            await ctx.groups.acceptInvite(urlCode).then(async result => {
                 await ctx.sendMessage(result, {
                     text: quote(`👋 Halo! Saya adalah Bot WhatsApp bernama ${config.bot.name}, dimiliki oleh ${config.owner.name}. Saya bisa melakukan banyak perintah, seperti membuat stiker, menggunakan AI untuk pekerjaan tertentu, dan beberapa perintah berguna lainnya. Saya di sini untuk menghibur dan menyenangkan Anda!`)
                 });
