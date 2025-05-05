444a94ad51780f6b02d5cc094c7d7788deb58b19
itsreimau <private.iblmln@gmail.com>
2025-05-05 23:58:52 +0700

    fix: Correct `mimetypes` import in animagine.js

--- commands/ai-image/animagine.js
+++ commands/ai-image/animagine.js
@@ -2,7 +2,7 @@ const {
     quote
 } = require("@mengkodingan/ckptw");
 const axios = require("axios");
-const mime = require("mimetypes");
+const mime = require("mime-types");
 
 module.exports = {
     name: "animagine",
