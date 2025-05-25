b14a7e0080db22508dd7f42b56333b9fe446195d
itsreimau <private.iblmln@gmail.com>
2025-05-25 07:18:05 +0700

    fix: Improve alkitab command formatting and update version

--- commands/tool/alkitab.js
+++ commands/tool/alkitab.js
@@ -5,35 +5,35 @@ const {
 const axios = require("axios");
 
 module.exports = {
-        name: "alkitab",
-        aliases: ["bible", "injil"],
-        category: "tool",
-        permissions: {
-            coin: 10
-        },
-        code: async (ctx) => {
-                const [passage, num] = ctx.args;
+    name: "alkitab",
+    aliases: ["bible", "injil"],
+    category: "tool",
+    permissions: {
+        coin: 10
+    },
+    code: async (ctx) => {
+        const [passage, num] = ctx.args;
 
-                if (!passage && !num) return await ctx.reply(
-                    `${quote(`${tools.cmd.generateInstruction(["send"], ["text"])}`)}\n` +
-                    `${quote(tools.cmd.generateCommandExample(ctx.used, "kej 2:18"))}\n` +
-                    quote(tools.cmd.generateNotes([`Ketik ${monospace(`${ctx.used.prefix + ctx.used.command} list`)} untuk melihat daftar.`]))
-                );
+        if (!passage && !num) return await ctx.reply(
+            `${quote(`${tools.cmd.generateInstruction(["send"], ["text"])}`)}\n` +
+            `${quote(tools.cmd.generateCommandExample(ctx.used, "kej 2:18"))}\n` +
+            quote(tools.cmd.generateNotes([`Ketik ${monospace(`${ctx.used.prefix + ctx.used.command} list`)} untuk melihat daftar.`]))
+        );
 
-                if (["l", "list"].includes(passage.toLowerCase())) {
-                    const listText = await tools.list.get("alkitab");
-                    return await ctx.reply(listText);
-                }
+        if (["l", "list"].includes(passage.toLowerCase())) {
+            const listText = await tools.list.get("alkitab");
+            return await ctx.reply(listText);
+        }
 
-                try {
-                    const apiUrl = tools.api.createUrl("https://api-alkitab.vercel.app", `/api/passage`, {
-                        passage,
-                        num
-                    });
-                    const result = (await axios.get(apiUrl)).data.bible.book;
+        try {
+            const apiUrl = tools.api.createUrl("https://api-alkitab.vercel.app", `/api/passage`, {
+                passage,
+                num
+            });
+            const result = (await axios.get(apiUrl)).data.bible.book;
 
-                    const resultText = result.chapter.verses.map(r =>
-                            `${quote(`Ayat: ${r.number}\n` +
+            const resultText = result.chapter.verses.map(r =>
+                `${quote(`Ayat: ${r.number}`)}\n` +
                 `${quote(`${r.text}`)}`
             ).join(
                 "\n" +
