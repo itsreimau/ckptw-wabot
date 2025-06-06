8b44232094cd84dfa72502516cdfc7ba95d0263a
itsreimau <private.iblmln@gmail.com>
2025-06-06 14:40:53 +0700

    feat: Add tomostchreal command

---
+++ commands/ai-misc/tomostchreal.js
@@ -0,0 +1,42 @@
+const {
+    quote
+} = require("@itsreimau/ckptw-mod");
+const axios = require("axios");
+const mime = require("mime-types");
+
+module.exports = {
+    name: "tomostchreal",
+    aliases: ["jadimosril", "jadimostchreal", "mosril", "tomosril"],
+    category: "ai-misc",
+    permissions: {
+        coin: 10
+    },
+    code: async (ctx) => {
+        const messageType = ctx.getMessageType();
+        const [checkMedia, checkQuotedMedia] = await Promise.all([
+            tools.cmd.checkMedia(messageType, "image"),
+            tools.cmd.checkQuotedMedia(ctx.quoted, "image")
+        ]);
+
+        if (!checkMedia && !checkQuotedMedia) return await ctx.reply(quote(tools.cmd.generateInstruction(["send", "reply"], "image")));
+
+        try {
+            const buffer = await ctx.msg.media.toBuffer() || await ctx.quoted.media.toBuffer();
+            const uploadUrl = await tools.general.upload(buffer, "image");
+            const result = tools.api.createUrl("http://vid2aud.hofeda4501.serv00.net", "/api/tomostchreal", {
+                url: uploadUrl
+            });
+            const result = (await axios.get(apiUrl)).data.result;
+
+            return await ctx.reply({
+                image: {
+                    url: result.url
+                },
+                mimetype: mime.lookup("jpeg"),
+                caption: result.message
+            });
+        } catch (error) {
+            return await tools.cmd.handleError(ctx, error, true);
+        }
+    }
+};
