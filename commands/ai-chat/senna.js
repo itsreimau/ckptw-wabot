338d9136422f0ab7524c47b02ad65f1533496dd3
itsreimau <private.iblmln@gmail.com>
2025-06-06 11:21:24 +0700

    feat: Add Senna command and increment version

--- commands/ai-chat/senna.js
+++ commands/ai-chat/senna.js
@@ -5,7 +5,7 @@ const axios = require("axios");
 
 module.exports = {
     name: "senna",
-    aliases: ["sennayapping"]
+    aliases: ["sennayapping"],
     category: "ai-chat",
     permissions: {
         coin: 10
