45d32ec640fa97ae212b3f83c30935a4a2fcb844
itsreimau <private.iblmln@gmail.com>
2025-06-23 17:22:34 +0700

    fix: Correct tagme command and increment version

--- commands/group/tagme.js
+++ commands/group/tagme.js
@@ -1,12 +1,13 @@
 module.exports = {
-        name: "tagme",
-        category: "group",
-        permissions: {
-            group: true
-        },
-        code: async (ctx) => {
-            return await ctx.reply({
-                text: `@${ctx.getId(ctx.sender.jid)}`,
-                mentions: [ctx.sender.jid]
-            });
-        };
+    name: "tagme",
+    category: "group",
+    permissions: {
+        group: true
+    },
+    code: async (ctx) => {
+        return await ctx.reply({
+            text: `@${ctx.getId(ctx.sender.jid)}`,
+            mentions: [ctx.sender.jid]
+        });
+    }
+};
