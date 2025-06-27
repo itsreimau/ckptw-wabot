5bde3a975fe27916e942ec7e6f11615a436940c3
itsreimau <private.iblmln@gmail.com>
2025-06-27 16:09:45 +0700

    Fix: Improve quoted message handling in input processing

--- commands/ai-image/netwrck.js
+++ commands/ai-image/netwrck.js
@@ -8,7 +8,7 @@ module.exports = {
         premium: true
     },
     code: async (ctx) => {
-        const input = ctx.args.join(" ") || ctx?.quoted?.conversation || (ctx.quoted && ((Object.values(ctx.quoted).find(v => v?.text || v?.caption) || {}).text || Object.values(ctx.quoted).find(v => v?.text || v?.caption)?.caption)) || null;
+        const input = ctx.args.join(" ") || ctx?.quoted?.conversation || (ctx.quoted && ((Object.values(ctx.quoted).find(v => v?.text || v?.caption)?.text) || (Object.values(ctx.quoted).find(v => v?.text || v?.caption)?.caption))) || null;
 
         if (!input) return await ctx.reply(
             `${formatter.quote(tools.msg.generateInstruction(["send"], ["text"]))}\n` +
