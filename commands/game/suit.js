e5971588bfb431242c911328ba3c4b5be4cdbaeb
itsreimau <private.iblmln@gmail.com>
2025-07-05 01:28:41 +0700

    fix: Correct message sending method in suit game

--- commands/game/suit.js
+++ commands/game/suit.js
@@ -67,7 +67,7 @@ module.exports = {
 
                 if (isGroup && participantId === accountId) {
                     if (participantAnswer === "accept") {
-                        await ctx.sendMessage(ctx.jid, {
+                        await ctx.sendMessage(m.id, {
                             text: formatter.quote(`@${accountId} menerima tantangan suit! Silahkan pilih di obrolan pribadi.`),
                             mentions: [accountJid]
                         }, {
